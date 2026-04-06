227d4c6a5be79661532486b16ad4e758bf219c16
Kn1ght072 <ivanecr07@gmail.com>
2026-04-06 20:21:18 -0300

    query selector

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// console.log("hola mundo");
+// titulo = document.querySelector('h1');
+// console.log(titulo);
+// titulo.innerHTML = '<i> este es mi titulo </i>';
+titulo = document.querySelector('h1').innerHTML = '<i> este es mi titulo </i>';
+document.querySelector('#caja').style.background = 'red';
+document.querySelector('.contenido').innerHTML = 'nuevo texto';
